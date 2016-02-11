9b155a69a6d5ef3988739f2cc815d2564f551ddf
russellvaughan <russellvaughan@gmail.com>
2016-02-11 16:04:40 +0000

    feature test complete, user can submit new link

--- app/app.rb
+++ app/app.rb
@@ -12,5 +12,14 @@ class Bookmark_Manager < Sinatra::Base
   	erb :links
   end
 
+  post '/links' do 
+  Link.create(url: params[:url] , title: params[:title])
+  redirect to ('/links')
+  end 
+
+  get '/links/new' do 
+  	erb :links_new
+  end
+
 run! if app_file == $0
 end
--- spec/feature/entering_links_spec.rb
+++ spec/feature/entering_links_spec.rb
@@ -7,11 +7,12 @@ require 'spec_helper'
 feature 'add site address and title' 	do
 
 	scenario 'to spend less time remembering a website, add site\'s address to my bookmark manager' do 
-	visit '/links'
+	visit '/links/new'
 	fill_in 'url', with: 'http://www.bbc.co.uk'
 	fill_in 'title', with: 'BBC News'
-	click_button('Save Link')	
-	expect(page).to have_content?('BBC News')
+	click_button('Create link')	
+	expect(current_path).to eq '/links'
+	expect(page).to have_content ('BBC News')
 	end
 
 end 
